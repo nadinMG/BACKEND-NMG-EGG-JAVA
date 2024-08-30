ec0a97634abbc391142917836368a6f6f2cdb977
NadinMG <nadin.gonzalez.746@gmail.com>
2024-08-30 16:57:14 -0300

    Update Ejercicio1.java

--- ENCUENTRO 2-3/Ejercicio1.java
+++ ENCUENTRO 2-3/Ejercicio1.java
@@ -10,13 +10,13 @@ public class Ejercicio1 {
     public static void main(String[] args) {
     
     Scanner leer = new Scanner(System.in);
-    System.out.println("INGRESE 2 NUMEROS ENTEROS PARA LA SUMA");
+    System.out.println("INGRESE 2 NUM ENTEROS PARA LA SUMA");
     int num1,num2;
     num1 = leer.nextInt();
     num2 = leer.nextInt();
     int suma;
     suma = num1 + num2;
-    System.out.println("LA SUMA ES DE: " + suma);
+    System.out.println("LA SUMA ES: " + suma);
     System.out.println("****");
     }
     
